913af00624dd5fe7a0d8cf909c7dfc1160cb6eed
Zizahswiftie <azizahulnha@gmail.com>
2026-03-10 13:50:38 +0700

    feat: add SqlClient namespace for database connection

--- AkademikADOApp/Form1.cs
+++ AkademikADOApp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+// Import SQL Client library
 using System.Data.SqlClient;
 
 namespace AkademikADOApp
